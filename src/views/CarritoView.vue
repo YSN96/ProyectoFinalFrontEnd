46e93e46b1969dd798eebbc1982ebed84856b400
abdellah yassin elhirch <ysn96abd@gmail.com>
2023-05-28 20:43:10 +0200

    Partie carrito completa

--- src/views/CarritoView.vue
+++ src/views/CarritoView.vue
@@ -15,26 +15,26 @@
                       class="fas fa-long-arrow-alt-left me-2"></i>Vuelve a elegir más Articulos</router-link></h5>
                 <hr>
 
-                <div class="card mb-3">
+                <div class="card mb-3" v-for="articul in pedidos" :key="articul.id_carrito">
                   <div class="card-body">
-                    <div class="d-flex justify-content-between" v-for="articul in pedidos" :key="articul.id_carrito">
+                    <div class="d-flex justify-content-between">
                       <div class="d-flex flex-row align-items-center">
                         <div>
                           <img
-                            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
-                            class="img-fluid rounded-3" alt="Shopping item" style="width: 65px;">
+                            :src="'http://127.0.0.1:8000/api/images/'+articul.archivo_imagen"
+                            class="img-fluid rounded-3" :alt="articul.nombre" style="width: 65px;">
                         </div>
                         <div class="ms-3">
-                          <h5>Iphone 11 pro</h5>
-                          <p class="small mb-0">256GB, Navy Blue</p>
+                          <h5>{{ articul.nombre }}</h5>
+                          <p> {{ articul.descripcion }} </p>
                         </div>
                       </div>
                       <div class="d-flex flex-row align-items-center">
                         <div style="width: 50px;">
-                          <h5 class="fw-normal mb-0">2</h5>
+                          <h5 class="fw-normal mb-0">{{ articul.cantidad }} </h5>
                         </div>
                         <div style="width: 80px;">
-                          <h5 class="mb-0">$900</h5>
+                          <h5 class="mb-0">{{ articul.precio * articul.cantidad }} €</h5>
                         </div>
                         <a href="#!" style="color: #ff0000;"><i class="fas fa-trash-alt"></i></a>
                       </div>
@@ -43,25 +43,15 @@
                 </div>
                 <div class="card bg-primary text-white rounded-3">
                   <div class="card-body">
-                    <div class="d-flex justify-content-between">
-                      <p class="mb-2">Subtotal</p>
-                      <p class="mb-2">$4798.00</p>
-                    </div>
-  
-                    <div class="d-flex justify-content-between">
-                      <p class="mb-2">Shipping</p>
-                      <p class="mb-2">$20.00</p>
-                    </div>
   
                     <div class="d-flex justify-content-between mb-4">
-                      <p class="mb-2">Total(Incl. taxes)</p>
-                      <p class="mb-2">$4818.00</p>
+                      <h4 class="mb-2">Total</h4>
+                      <h4 class="mb-2">{{ total }} €</h4>
                     </div>
   
                     <button type="button" class="btn btn-info btn-block btn-lg">
                       <div class="d-flex justify-content-between">
-                        <span>$4818.00</span>
-                        <span>Checkout <i class="fas fa-long-arrow-alt-right ms-2"></i></span>
+                        <span>Pagar <i class="fas fa-long-arrow-alt-right ms-2"></i></span>
                       </div>
                     </button>
   
@@ -88,6 +78,7 @@
     data() {
       return {
         pedidos:[],
+        total: 0,
       };
     },
     methods: {
@@ -103,7 +94,6 @@
         const result = await response.json();
         console.log(result);
         this.pedidos = result;
-        console.log('OK')
       },
       async articulosUsuario(id_concepto, id_usuario){
         const token = localStorage.getItem('user-token');
@@ -133,9 +123,18 @@
         } else {
           this.articulosUsuario(concepto, idUsuario);
         }
+
+        this.total = 0;
+        for (let i = 0; i < this.pedidos.length; i++) {
+          this.total += this.pedidos.precio * this.pedidos.cantidad;
+        }
+        console.log(this.total);
       } else {
         this.$router.push({ name: 'LoginView' });
       }
+    },
+    created() {
+      
     }, 
   }
   </script>
